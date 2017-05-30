4a89812347c39c2e471e4692835b3c2bb9221413
ASIF008 <ankitkat88@gmail.com>
2017-05-30 16:22:54 +0600

    add favourite favourite and share

--- app/src/main/java/com/example/android/appbarapp/MainActivity.java
+++ app/src/main/java/com/example/android/appbarapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.android.appbarapp;
 
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,32 @@ public class MainActivity extends AppCompatActivity {
         /*return super.onCreateOptionsMenu(menu);*/
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
+        /*return true;*/
+
+        // Define the listener
+        MenuItemCompat.OnActionExpandListener expandListener = new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Toast.makeText(getApplicationContext(), "collapse", Toast.LENGTH_SHORT).show();
+                return true;  // Return true to collapse action view
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                // Do something when expanded
+                Toast.makeText(getApplicationContext(), "Expanded", Toast.LENGTH_SHORT).show();
+                return true;  // Return true to expand action view
+            }
+        };
+
+        // Get the MenuItem for the action item
+        MenuItem actionMenuItem = menu.findItem(R.id.action_search);
+
+        // Assign the listener to that action item
+        MenuItemCompat.setOnActionExpandListener(actionMenuItem, expandListener);
+
+        // Any other things you have to do when creating the options menu…
+
         return true;
     }
 
@@ -37,8 +65,14 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_favorite:
                 // User chose the "Favorite" action, mark the current item
                 // as a favorite...
+                Toast.makeText(this, "love clicked", Toast.LENGTH_SHORT).show();
                 return true;
+            case R.id.action_search:
 
+                return true;
+            case R.id.action_share:
+                Toast.makeText(this, "share clicked", Toast.LENGTH_SHORT).show();
+                return true;
             default:
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
